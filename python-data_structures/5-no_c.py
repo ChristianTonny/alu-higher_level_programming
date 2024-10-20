e0bc90a72eed3f8fb32a93dfcb1b8942156c0415
ChristianTonny <g.iradukund1@alustudent.com>
2024-10-20 05:28:03 -0700

    Fix 5-no_c.py to handle all test cases without using str.replace()

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    new_list = my_list.copy()
-    new_list[idx] = element
-    return new_list
+def no_c(my_string):
+    return ''.join(char for char in my_string if char.lower() not in 'c')
