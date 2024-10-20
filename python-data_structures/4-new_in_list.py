cea0d399656c92925ca1b72e24310343f08b1bfa
ChristianTonny <g.iradukund1@alustudent.com>
2024-10-20 05:29:55 -0700

    Fix 4-new_in_list.py to handle all test cases without using try/except

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        for num in reversed(my_list):
-            print("{:d}".format(num))
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
