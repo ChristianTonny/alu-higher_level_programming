a6edfcd1f72644c5fb1f651a9a81dc57f0d5d6a2
ChristianTonny <g.iradukund1@alustudent.com>
2024-10-20 04:42:20 -0700

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     total = 0
     for arg in sys.argv[1:]:
         try:
             total += int(arg)
         except ValueError:
             pass  # Ignore non-integer arguments
-
     print(total)#!/usr/bin/python3
