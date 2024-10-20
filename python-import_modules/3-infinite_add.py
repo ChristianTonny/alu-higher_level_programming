a15f77c68b66ad5cd55ef0aa45d4da489c55bf2a
ChristianTonny <g.iradukund1@alustudent.com>
2024-10-20 04:36:02 -0700

    Added idiom if

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import sys
-
-total = sum(int(arg) for arg in sys.argv[1:])
-print(total)
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
