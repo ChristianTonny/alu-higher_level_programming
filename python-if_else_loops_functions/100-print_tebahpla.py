b3b42ce8f38dd0b69a5704f83f228bd14745024c
ChristianTonny <g.iradukund1@alustudent.com>
2024-10-20 04:05:34 -0700

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join([chr((i - 97 + 13) % 26 + 97 + (32 if i % 2 else 0)) for i in range(122, 96, -1)]), end="")
+for i in range(122, 96, -1):
+    print("{}".format(chr(i - 32) if i % 2 == 0 else chr(i)), end="")
