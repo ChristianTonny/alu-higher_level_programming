a51228e8bd14add280e077aaa3c37a10475ebb1d
ChristianTonny <g.iradukund1@alustudent.com>
2024-11-19 14:05:24 +0000

    4-headerrr

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request with a header to the specified URL, targeting route_5
+# Send a GET request with a header to the correct route
 curl -s -H "X-HolbertonSchool-User-Id: 98" "http://$1/route_5"
