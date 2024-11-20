9b0cab9353ad0c023d1b7d9b03bcafc8636ab108
ChristianTonny <g.iradukund1@alustudent.com>
2024-11-20 11:22:44 +0200

    Changed url

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.aluswe.com/status."""
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.aluswe.com/status")
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
