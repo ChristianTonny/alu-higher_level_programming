4592f5e9f41a0585393e108551462b6bce75a5dd
ChristianTonny <g.iradukund1@alustudent.com>
2024-12-03 06:57:28 +0200

    statusss

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,29 +5,34 @@ A script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    try:
-        if len(sys.argv) != 4:
-            print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-            sys.exit(1)
 
-        con = MySQLdb.connect(
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
+            user=username,
+            passwd=password,
+            db=database
         )
 
-        with con.cursor() as cur:
-            cur.execute("SELECT * FROM states ORDER BY id ASC")
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        states = cursor.fetchall()
+
+        for state in states:
+            print(state)
 
     except MySQLdb.Error as e:
         print("MySQL Error: {}".format(e))
         sys.exit(1)
+
     finally:
-        if 'con' in locals() and con:
-            con.close()
+        if 'db' in locals():
+            db.close()
