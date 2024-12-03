0a320ef7e476fe78d743e87cb2310e3730cee82e
ChristianTonny <g.iradukund1@alustudent.com>
2024-12-03 06:55:18 +0200

    statusss

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
 """
-a script that lists all states from the database hbtn_0e_0_usa:
+A script that lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
 
-
 if __name__ == '__main__':
-    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
-    with con.cursor() as cur:
-        """Used context manager to automatically close the cursor object"""
-        cur.execute('SELECT * FROM states ORDER BY states.id;')
-        [print(row) for row in cur.fetchall()]
-    con.close()
+    try:
+        if len(sys.argv) != 4:
+            print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+            sys.exit(1)
+
+        con = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+        with con.cursor() as cur:
+            cur.execute("SELECT * FROM states ORDER BY id ASC")
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
+    finally:
+        if 'con' in locals() and con:
+            con.close()
