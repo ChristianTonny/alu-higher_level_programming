5f70408f19541a8925c76f6ecf8eb64a4c020fd1
ChristianTonny <g.iradukund1@alustudent.com>
2024-12-03 06:32:14 +0200

    status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""Fetched from https://intranet.hbtn.io/status"""
+"""  fetches https://alu-intranet.hbtn.io/status  """
 import urllib.request
 
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = "https://alu-intranet.hbtn.io/status"
 
 if __name__ == "__main__":
-    try:
-        request = urllib.request.Request("https://intranet.hbtn.io/status")
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
-    except:
-        request = urllib.request.Request("http://0.0.0.0:5050/status")
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode("utf-8"))
