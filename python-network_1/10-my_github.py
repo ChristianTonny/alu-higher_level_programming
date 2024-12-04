a527144523c45234752d389847724a91610fde5c
ChristianTonny <g.iradukund1@alustudent.com>
2024-12-04 16:24:36 +0200

    Added stuff

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -3,34 +3,13 @@
 Python script that takes GitHub credentials (username and token)
 and uses the GitHub API to display user id
 """
+from requests import get, auth
 import sys
-import requests
 
 
 if __name__ == "__main__":
-    # Get username and token from command line arguments
-    username = sys.argv[1]
-    token = sys.argv[2]
-
-    # GitHub API endpoint for user information
-    url = f"https://api.github.com/users/{username}"
-
-    # Set up the authentication using Basic Auth with token
-    # The token is used as the password
-    auth = (username, token)
-
-    try:
-        # Make GET request to GitHub API with authentication
-        response = requests.get(url, auth=auth)
-
-        # Check if request was successful
-        if response.status_code == 200:
-            # Extract and print the user id from the response
-            print(response.json().get('id'))
-        else:
-            # If authentication fails or user not found, print None
-            print(None)
-
-    except requests.exceptions.RequestException:
-        # Handle any requests-related exceptions
-        print(None)
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
