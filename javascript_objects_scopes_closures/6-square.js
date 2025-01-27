5a066f15819d8349175c45cfb79303ff4c757e0a
ChristianTonny <g.iradukund1@alustudent.com>
2025-01-27 10:01:41 +0200

    closures

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-const Square = require('./5-square');
-
-class SquareWithChar extends Square {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    const char = c || 'X';
-    for (let i = 0; i < this.height; i++) {
-      console.log(char.repeat(this.width));
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
-}
-
-module.exports = SquareWithChar;
+};
